a71875647aa1391d8a0bb912e07227444e24154e
drewjbartlett <drewb9@gmail.com>
2018-06-11 14:33:03 -0700

    Fix multiple tests for cell

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,30 +2,46 @@ require_relative '../lib/cell'
 
 describe "A cell" do
 
+  let (:cell) { Cell.new }
+  let (:dead_cell) {
+    dead_cell = Cell.new
+    dead_cell.die
+
+    dead_cell
+  }
+
   it 'exists' do
     Cell.new
   end
 
-  xit 'is initially alive' do
-
+  it 'is initially alive' do
+    expect(cell).to be_alive
   end
 
-  xit 'can die' do
-
+  it 'can die' do
+    cell.die
+    expect(cell).to be_dead
   end
 
-  xit 'has neighbors, which is initially an empty array' do
-
+  it 'has neighbors, which is initially an empty array' do
+    expect(cell.neighbors).to eq([])
   end
 
-  xit 'has a tick method' do
-
+  it 'has a tick method' do
+    expect(cell).to respond_to(:tick)
   end
 
   describe 'after a tick' do
 
-    xit 'a live cell with fewer than two live neighbors dies' do
+    it 'a live cell with fewer than two live neighbors dies' do
+      neighbor_cell = Cell.new
+      neighbor_cell.add_neighbor(cell)
 
+      cell.add_neighbor(neighbor_cell)
+
+      cell.tick
+
+      expect(cell).to be_dead
     end
 
     xit 'a dead cell with fewer than two live neighbors stays dead' do
