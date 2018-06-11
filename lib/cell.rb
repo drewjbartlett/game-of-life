40dc214f13f023cc547cf747c7512e1e1edd1fee
drewjbartlett <drewb9@gmail.com>
2018-06-11 14:43:04 -0700

    Add commented out tests. Add cell class that was not previously committed

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,1 +1,40 @@
 # TODO
+class Cell
+  MINIMUM_LIVE_NEIGHBORS_TO_DIE = 2
+
+  attr_accessor :neighbors
+
+  def initialize
+    @alive = true
+    @neighbors = []
+  end
+
+  def alive?
+    @alive
+  end
+
+  def dead?
+    !@alive
+  end
+
+  def die
+    @alive = false
+  end
+
+  def tick
+    if (self.live_neighbors.length < MINIMUM_LIVE_NEIGHBORS_TO_DIE)
+      self.die
+    end
+  end
+
+  def add_neighbor(neighbor)
+    @neighbors << neighbor
+  end
+
+  def live_neighbors
+    @neighbors.select do |cell|
+      cell.alive?
+    end
+  end
+
+end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,34 +1,34 @@
-require_relative '../lib/grid'
+# require_relative '../lib/grid'
 
-describe "A grid of cells" do
+# describe "A grid of cells" do
 
-  it 'exists' do
-    Grid.new
-  end
+#   it 'exists' do
+#     Grid.new
+#   end
 
-  xit 'needs a width and height to be created' do
+#   xit 'needs a width and height to be created' do
 
-  end
+#   end
 
-  xit 'exposes a collection of cells' do
+#   xit 'exposes a collection of cells' do
 
-  end
+#   end
 
-  xit 'has a tick method' do
+#   xit 'has a tick method' do
 
-  end
+#   end
 
-  xit 'the tick method changes the state of the grid' do
+#   xit 'the tick method changes the state of the grid' do
 
-  end
+#   end
 
-  # o..
-  # ...
-  # ...
-  xit 'a grid with one living cell should only have dead cells after a tick' do
+#   # o..
+#   # ...
+#   # ...
+#   xit 'a grid with one living cell should only have dead cells after a tick' do
 
-  end
+#   end
 
-  # TODO: Test the cell rules in the context of a grid/collection of cells.
+#   # TODO: Test the cell rules in the context of a grid/collection of cells.
 
-end
+# end
